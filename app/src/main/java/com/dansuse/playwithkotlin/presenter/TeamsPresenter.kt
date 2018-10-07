de90d750b8ddb38fff80a97f1931e70acaa190c1
Daniel <danielsugiarto89@gmail.com>
2018-10-07 19:27:17 +0700

    Selesai modul 8 kotlin coroutines

--- app/src/main/java/com/dansuse/playwithkotlin/presenter/TeamsPresenter.kt
+++ app/src/main/java/com/dansuse/playwithkotlin/presenter/TeamsPresenter.kt
@@ -5,6 +5,11 @@ import com.dansuse.playwithkotlin.repository.ApiRepository
 import com.dansuse.playwithkotlin.repository.TheSportDBApi
 import com.dansuse.playwithkotlin.view.TeamsView
 import com.google.gson.Gson
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.bg
+
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -14,14 +19,22 @@ class TeamsPresenter (
     private val gson: Gson){
   fun getTeamList(league: String?){
     view.showLoading()
-    doAsync {
-      val data = gson.fromJson(apiRepository.doRequest(TheSportDBApi.getTeams(league)),
-          TeamResponse::class.java)
-
-      uiThread {
+//    doAsync {
+//      val data = gson.fromJson(apiRepository.doRequest(TheSportDBApi.getTeams(league)),
+//          TeamResponse::class.java)
+//
+//      uiThread {
+//        view.hideLoading()
+//        view.showTeamList(data.teams)
+//      }
+//    }
+    async(UI){
+        val data: Deferred<TeamResponse> = bg {
+            gson.fromJson(apiRepository.doRequest(TheSportDBApi.getTeams(league)),
+                    TeamResponse::class.java)
+        }
         view.hideLoading()
-        view.showTeamList(data.teams)
-      }
+        view.showTeamList(data.await().teams)
     }
   }
 }
--- app/src/main/java/com/dansuse/playwithkotlin/teamdetail/TeamDetailPresenter.kt
+++ app/src/main/java/com/dansuse/playwithkotlin/teamdetail/TeamDetailPresenter.kt
@@ -4,6 +4,10 @@ import com.dansuse.playwithkotlin.model.TeamResponse
 import com.dansuse.playwithkotlin.repository.ApiRepository
 import com.dansuse.playwithkotlin.repository.TheSportDBApi
 import com.google.gson.Gson
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.bg
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -12,15 +16,26 @@ class TeamDetailPresenter(private val view: TeamDetailView,
                           private val gson:Gson){
     fun getTeamDetail(teamId:String){
         view.showLoading()
-        doAsync {
-            val data = gson.fromJson(
-                     apiRepository.doRequest(
-                            TheSportDBApi.getTeamDetail(teamId)),
-                     TeamResponse::class.java)
-            uiThread {
-                view.hideLoading()
-                view.showTeamDetail(data.teams)
+//        doAsync {
+//            val data = gson.fromJson(
+//                     apiRepository.doRequest(
+//                            TheSportDBApi.getTeamDetail(teamId)),
+//                     TeamResponse::class.java)
+//            uiThread {
+//                view.hideLoading()
+//                view.showTeamDetail(data.teams)
+//            }
+//        }
+
+        async(UI){
+            val data: Deferred<TeamResponse> = bg{
+                gson.fromJson(
+                        apiRepository.doRequest(
+                                TheSportDBApi.getTeamDetail(teamId)),
+                        TeamResponse::class.java)
             }
+            view.hideLoading()
+            view.showTeamDetail(data.await().teams)
         }
     }
 }
